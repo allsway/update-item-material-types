63e32bfc877cccbcecb1d9fc88df6049a2831ec1
Sarina <ssinick@calstate.edu>
2016-08-29 12:56:13 -0700

    Update update_mattype.php

--- update_mattype.php
+++ update_mattype.php
@@ -1,7 +1,8 @@
 <?php
 
-
-
+	/*
+		Gets the item from the Alma item GET API
+	*/
 	function getxml($url)
 	{
 		$curl = curl_init();
@@ -21,11 +22,13 @@
 			else
 			{	
 				return $xml;
-        		}
-		 }
-
+	        	}
+	        }
 	}
 
+	/*
+		Call to the Alma PUT API to update the item material type
+	*/
 	function putxml($url,$body)
 	{
 		$curl = curl_init($url);
@@ -46,7 +49,7 @@
 			else
 			{
 				return $xml;
-        		}			
+        	}			
 		}
 		catch(Exception $exception)
 		{
@@ -57,6 +60,9 @@
 
 	}
 	
+	/*
+		Parses the ITYPE from the migrated item note field, and returns only the ITYPE number
+	*/
 	function get_itype_num($string)
 	{
 		$itypes = explode(':',$string);
@@ -65,6 +71,9 @@
 		return $itype_val;
 	}
 	
+	/*
+		Turns the parsed simple XML into XML that can be returned in the PUT function
+	*/
 	function make_xml($xml)
 	{
 		$doc = new DOMDocument();
@@ -74,7 +83,9 @@
 		return $return_xml;
 	}
 	
-	
+	/*
+		Reads in campus Alma parameters
+	*/
 	$ini_array = parse_ini_file("update_mattype.ini");
 
 	$key= $ini_array['apikey'];
@@ -90,87 +101,100 @@
 	*/
 
 	  	$itype_mapping = fopen($argv[1],"r");
-	  	
-	  	foreach (glob("itemdirectory/*") as $file) 
-		{
+		$items_file = fopen($argv[2],"r");		
+		$itype2mattype = array();
 	
-			$items_file = fopen($file,"r");		
-			$itype2mattype = array();
-		
+		/*
+			Read in itype to alma mat type mapping
+		*/
+		while(($line = fgetcsv($itype_mapping)) !== FALSE)
+		{
+			$itype2mattype[$line[0]] = $line[1];
+		}
+		fclose($itype_mapping);
+	
+		var_dump($itype2mattype);
+	
+	
+		/*
+			Read in each line of the item csv file
+			Call API for every item to get the XML for the item.  
+			Get current itype value from mapped note field (may vary based on campus)
+			Use mapping array to get the Alma material type for each ITYPE value
+			PUT call to Alma API to replace the item material type
+		*/
+		$flag = true;
+		while (($line = fgetcsv($items_file)) !== FALSE) 
+		{
+			// Ignores the first line of the file, the header
+			if($flag) { $flag = false; continue; }
+	
 			/*
-				Read in itype to alma mat type mapping
+				Get info to call API
 			*/
-			while(($line = fgetcsv($itype_mapping)) !== FALSE)
-			{
-				$itype2mattype[$line[0]] = $line[1];
-			}
-			fclose($itype_mapping);
-		
-			var_dump($itype2mattype);
-		
-		
-			/*
-				Read in each line of the item csv file
-				Call API for every item to get the XML for the item.  
-				Get current itype value from internal note 3 field
-				Use mapping set up to get the Alma material type for each ITYPE value
-				PUT call to Alma API to replace the item material type
-			*/
-			$flag = true;
-			while (($line = fgetcsv($items_file)) !== FALSE) 
-			{
-				if($flag) { $flag = false; continue; }
-		
-				/*
-					Get info to call API
-				*/
-				$bib_id = $line[0];
-				$holding_id = $line[1];
-				$item_id = $line[2];
-
-				$link =  '/almaws/v1/bibs/'.$bib_id.'/holdings/'.$holding_id.'/items/'.$item_id;
-				$url = $baseurl . $link . '?apikey=' . $key;
-				$xml = getxml($url);
-
-				/*
-					Get new mat type, and replace current mat type with new mat type
-				*/
-				$current_mat_type = $xml->item_data->physical_material_type;
-				// Will vary based on where campuses migrate the ITYPE to
-				$itype_value = $xml->item_data->statistics_note_2;
-				$itype_value = get_itype_num($itype_value);
+			$bib_id = $line[0];
+			$holding_id = $line[1];
+			$item_id = $line[2];
 			
-				if(isset($itype2mattype[$itype_value]))
-				{
-					$new_mattype = $itype2mattype[$itype_value];
-					echo $xml->item_data->pid . " " . $current_mat_type . " " . $itype_value . " " . $new_mattype . PHP_EOL;
-					if(trim($new_mattype.'', ' ') != trim($current_mat_type.'', ' '))
-					{
-						$xml->item_data->physical_material_type = $new_mattype;
-						$xml = make_xml($xml);
-						$result = putxml($url,$xml);
-						var_dump($result);
+			// Use items CSV file to get the item information, and get XML for item from the Alma items API
+			$link =  '/almaws/v1/bibs/'.$bib_id.'/holdings/'.$holding_id.'/items/'.$item_id;
+			$url = $baseurl . $link . '?apikey=' . $key;
+			$xml = getxml($url);
 
-					}
-					else
-					{
-						echo  "Mat types are equal" .  PHP_EOL;
-					}
+			/*
+				Get new mat type, and replace current mat type with new mat type
+			*/
+			$current_mat_type = $xml->item_data->physical_material_type;
+			// Will vary based on where campuses migrate the ITYPE to
+			$itype_value = $xml->item_data->internal_note_3;
+			$itype_value = get_itype_num($itype_value);
+		
+			// Not all itypes map directly to an item material type.  If there is no mapping, there is no update, and no PUT request		
+			if(isset($itype2mattype[$itype_value]))
+			{
+				// Set new material type
+				$new_mattype = $itype2mattype[$itype_value];
+				echo $xml->item_data->pid . " " . $current_mat_type . " " . $itype_value . " " . $new_mattype . PHP_EOL;
+				// Check if mapped material type is the same as current material type
+				if(trim($new_mattype.'', ' ') != trim($current_mat_type.'', ' '))
+				{
+					// Set to new material type
+					// Make XML ready for PUT request
+					// Make PUT request
+					$xml->item_data->physical_material_type = $new_mattype;
+					$xml = make_xml($xml);
+					$result = putxml($url,$xml);
+					var_dump($result);
+
 				}
+				// If current material type is the same as mapped material type, no need for PUT request, no changes made
 				else
 				{
-					// Do nothing - there isn't an update required 
-				}	
+					echo  "Mat types are equal" .  PHP_EOL;
+				}
+			}
+			else
+			{
+				echo "Item not updated";
+				// Do nothing - there isn't an update required 
+			}	
 			
 
 	
-			}
-				print $items_file . PHP_EOL;
-				fclose($items_file);
-
 		}
+			print $items_file . PHP_EOL;
+			fclose($items_file);
 		
 ?>
+
+
+
+
+
+
+
+
+
 
 
 
